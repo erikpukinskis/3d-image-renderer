af74afa12fba5d17e1d02288e505ebc09ccf1da8
Erik Pukinskis <erik@snowedin.net>
2025-02-28 12:09:32 -0800

    Add README, some vscode stuff

--- src/Renderer.tsx
+++ src/Renderer.tsx
@@ -1,6 +1,27 @@
 const CANVAS_WIDTH = 300;
 const CANVAS_HEIGHT = 300;
 
+const FULL_SCREEN_QUAD = new Float32Array([
+  -1.0,
+  -1.0,
+  0.0, // bottom left
+  1.0,
+  -1.0,
+  0.0, // bottom right
+  -1.0,
+  1.0,
+  0.0, // top left
+  -1.0,
+  1.0,
+  0.0, // top left
+  1.0,
+  -1.0,
+  0.0, // bottom right
+  1.0,
+  1.0,
+  0.0, // top right
+]);
+
 export const Renderer: React.FC = () => {
   const render = (canvas: HTMLCanvasElement) => {
     if (!canvas) return;
@@ -18,28 +39,15 @@ export const Renderer: React.FC = () => {
 
     const shaderProgram = createShaderProgram(gl);
 
-    // useProgram is similar to bindBuffer, since we can only have one program going at a time we need to tell OpenGL which is up.
+    // useProgram is similar to bindBuffer, since we can only have one program
+    // going at a time we need to tell OpenGL which is up.
     gl.useProgram(shaderProgram);
-
-    gl.clearColor(1, 1, 1, 1);
-
-    gl.enable(gl.DEPTH_TEST);
-
     gl.viewport(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
     // Draw
-    var vertices = new Float32Array([
-        -1.0, -1.0, 0.0,  // bottom left
-        1.0, -1.0, 0.0,   // bottom right
-        -1.0, 1.0, 0.0,   // top left
-        -1.0, 1.0, 0.0,   // top left
-        1.0, -1.0, 0.0,   // bottom right
-        1.0, 1.0, 0.0     // top right
-    ]);
-
     var vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, FULL_SCREEN_QUAD, gl.STATIC_DRAW);
 
     gl.vertexAttribPointer(
       gl.getAttribLocation(shaderProgram, "vertexData"),
@@ -51,7 +59,9 @@ export const Renderer: React.FC = () => {
     );
 
     // Enable the vertex attribute array
-    gl.enableVertexAttribArray(gl.getAttribLocation(shaderProgram, "vertexData"));
+    gl.enableVertexAttribArray(
+      gl.getAttribLocation(shaderProgram, "vertexData")
+    );
 
     console.log("Drawing screen quad triangles");
     gl.drawArrays(gl.TRIANGLES, 0, 6);
@@ -69,8 +79,9 @@ function createShaderProgram(gl: WebGLRenderingContext) {
     throw new Error("Could not create WebGL program");
   }
 
-  // The vertex shader is what tells the GPU where our verticies are, based on whatever world data we feed it
-  var vertexShader = createSimpleVertexShader(gl);
+  // The vertex shader is what tells the GPU where our verticies are, based on
+  // whatever world data we feed it
+  var vertexShader = createTriangleShader(gl);
 
   // The fragment shader renders all of the pixels inside that geometry
   var fragmentShader = createFragmentShader(gl);
@@ -79,14 +90,22 @@ function createShaderProgram(gl: WebGLRenderingContext) {
 
   gl.attachShader(program, fragmentShader);
 
-  // The program has to be "linked" before it can be "used". There's not a lot of documentation out there, but it seems to be another kind of compile step that just checks that the vertex and fragment shaders are both there and make sense together.
-  // Also, after the program is in use we can modify the individual shaders, but those changes won't take effect until we call linkProgram again.
+  // The program has to be "linked" before it can be "used". There's not a lot
+  // of documentation out there, but it seems to be another kind of compile step
+  // that just checks that the vertex and fragment shaders are both there and
+  // make sense together. Also, after the program is in use we can modify the
+  // individual shaders, but those changes won't take effect until we call
+  // linkProgram again.
   gl.linkProgram(program);
 
   return program;
 }
 
-function createSimpleVertexShader(gl: WebGLRenderingContext) {
+/**
+ * This is pretty much the simplest possible vertex shader. It just proxies
+ * through verticies.
+ */
+function createTriangleShader(gl: WebGLRenderingContext) {
   const FULL_SCREEN_QUAD = `
         attribute vec4 vertexData;
 
@@ -102,15 +121,15 @@ function createSimpleVertexShader(gl: WebGLRenderingContext) {
   }
 
   gl.shaderSource(vertexShader, FULL_SCREEN_QUAD);
-
-  // The compile step takes the source code we provided and builds an executable "Program Object". We could also write multiple strings to the shader with multiple shaderSource calls before compiling, if we wanted to use some shared code.
   gl.compileShader(vertexShader);
-
   checkCompileStatus(gl, vertexShader);
 
   return vertexShader;
 }
 
+/**
+ * Since we're ray casting, the fragment shader is doing the bulk of the work.
+ */
 function createFragmentShader(gl: WebGLRenderingContext) {
   const ALL_GREEN_EVERYTHING = `
         precision mediump float;
@@ -127,9 +146,7 @@ function createFragmentShader(gl: WebGLRenderingContext) {
   }
 
   gl.shaderSource(fragmentShader, ALL_GREEN_EVERYTHING);
-
   gl.compileShader(fragmentShader);
-
   checkCompileStatus(gl, fragmentShader);
 
   return fragmentShader;
