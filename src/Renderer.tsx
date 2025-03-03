e562165138d53e5b7ff77542a9d7d49f4618c028
Erik Pukinskis <erik@snowedin.net>
2025-03-03 09:50:38 -0800

    Drag to rotate (#2)
    * Add a projection transform that moves the camera back a bit
    
    * Constants that rotate in x- and y-direction
    
    * Drag to rotate
    
    * Adjust FOV

--- src/Renderer.tsx
+++ src/Renderer.tsx
@@ -1,6 +1,21 @@
+// Note: using gl-matrix 4.0-beta
+import { Mat4 } from "gl-matrix";
+import React, { useRef, useState } from "react";
+
 const CANVAS_WIDTH = 300;
 const CANVAS_HEIGHT = 300;
 
+/**
+ * We scale up the Y rotation a little so that you can do a full 180 in a single drag
+ */
+const X_ROTATION_SPEED = 1;
+const Y_ROTATION_SPEED = 3;
+
+/**
+ * Camera definition
+ */
+const CAMERA_DISTANCE = -6;
+const CAMERA_FOV = 0.2;
 const FULL_SCREEN_QUAD = new Float32Array([
   -1.0,
   -1.0,
@@ -22,12 +37,40 @@ const FULL_SCREEN_QUAD = new Float32Array([
   0.0, // top right
 ]);
 
+/**
+ * These are world-relative transforms that will apply to the whole world, which
+ * is a little confusing. I am not super clear on what are best practices for
+ * this matrix. Seems like gl-matrix uses a lookAt helper, which makes this
+ * whole question go away. But I would like to understand it deeply.
+ */
+type Camera = {
+  tx: number;
+  ty: number;
+  tz: number;
+  xRotation: number;
+  yRotation: number;
+};
+
 export const Renderer: React.FC = () => {
-  const render = (canvas: HTMLCanvasElement) => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  const cameraRef = useRef<Camera>({
+    tx: 0,
+    ty: 0,
+    tz: CAMERA_DISTANCE,
+    xRotation: 0,
+    yRotation: 0,
+  });
+
+  const handleMount = (canvas: HTMLCanvasElement | null) => {
+    canvasRef.current = canvas;
+    render();
+  };
+
+  const render = () => {
+    const canvas = canvasRef.current;
+
     if (!canvas) return;
-
-    console.log("Rendering canvas");
-
     // Initialize the GL context
     const gl = canvas.getContext("webgl", {
       antialias: false,
@@ -52,17 +95,128 @@ export const Renderer: React.FC = () => {
     gl.uniform2f(resolutionLocation, CANVAS_WIDTH, CANVAS_HEIGHT);
 
     // Set the quad uniforms
-    const p00Location = gl.getUniformLocation(shaderProgram, "uP00");
-    gl.uniform3f(p00Location, -1, -1, -2);
+    const quadLocation = gl.getUniformLocation(shaderProgram, "uQuad");
+    gl.uniform3fv(
+      quadLocation,
+      [
+        // p00
+        -1, -1, 0,
+        // p01
+        -1, 1, 0,
+        // p10,
+        1, -1, 0,
+        // p11
+        1, 1, 0,
+      ]
+    );
 
-    const p01Location = gl.getUniformLocation(shaderProgram, "uP01");
-    gl.uniform3f(p01Location, -1, 1, -2);
+    // Set the camera uniform(s)
+    // ————————————————————————
+    const { tx, ty, tz, xRotation, yRotation } = cameraRef.current;
 
-    const p10Location = gl.getUniformLocation(shaderProgram, "uP10");
-    gl.uniform3f(p10Location, 1, -1, -2);
+    /**
+     * Translation in 3D:
+     * https://www.youtube.com/watch?v=bW9goiYaOBs
+     *
+     *    1  0  0  tx
+     *    0  1  0  ty
+     *    0  0  1  tz
+     *    0  0  0  1
+     */
+    const translationMatrix = new Mat4(
+      // x column
+      1,
+      0,
+      0,
+      0,
+      // y column
+      0,
+      1,
+      0,
+      0,
+      // z column
+      0,
+      0,
+      1,
+      0,
+      // w column
+      tx,
+      ty,
+      tz,
+      1
+    );
 
-    const p11Location = gl.getUniformLocation(shaderProgram, "uP11");
-    gl.uniform3f(p11Location, 1, 1, -2);
+    /**
+     * Rotation about the y-axis by angle a (radians):
+     * https://en.wikipedia.org/wiki/Rotation_matrix#Basic_3D_rotations
+     *
+     * cos(a)   0   sin(a)
+     *   0      1     0
+     * -sin(a)  0   cos(a)
+     */
+    const yRotationMatrix = new Mat4(
+      // x column
+      Math.cos(yRotation),
+      0,
+      -1 * Math.sin(yRotation),
+      0,
+      // y column
+      0,
+      1,
+      0,
+      0,
+      // z column
+      Math.sin(yRotation),
+      0,
+      Math.cos(yRotation),
+      0,
+      // w column
+      0,
+      0,
+      0,
+      1
+    );
+
+    /**
+     * Rotation about the y-axis by angle a (radians):
+     * https://en.wikipedia.org/wiki/Rotation_matrix#Basic_3D_rotations
+     *
+     *   1     0       0
+     *   0   cos(a)  -sin(a)
+     *   0   sin(a)   cos(a)
+     */
+    const xRotationMatrix = new Mat4(
+      // x column
+      1,
+      0,
+      0,
+      0,
+      // y column
+      0,
+      Math.cos(xRotation),
+      Math.sin(xRotation),
+      0,
+      // z column
+      0,
+      -1 * Math.sin(xRotation),
+      Math.cos(xRotation),
+      0,
+      // w column
+      0,
+      0,
+      0,
+      1
+    );
+
+    const projectionLocation = gl.getUniformLocation(
+      shaderProgram,
+      "uProjection"
+    );
+
+    const projectionMatrix = new Mat4();
+    Mat4.multiply(projectionMatrix, translationMatrix, yRotationMatrix);
+    Mat4.multiply(projectionMatrix, projectionMatrix, xRotationMatrix);
+    gl.uniformMatrix4fv(projectionLocation, false, projectionMatrix);
 
     // Draw
     const vertexBuffer = gl.createBuffer();
@@ -81,12 +235,79 @@ export const Renderer: React.FC = () => {
 
     gl.enableVertexAttribArray(positionLocation);
 
-    console.log("Drawing screen quad triangles");
     gl.drawArrays(gl.TRIANGLES, 0, 6);
   };
 
+  const [grabbing, setGrabbing] = useState(false);
+
+  const handleMouseDown = (down: React.MouseEvent) => {
+    setGrabbing(true);
+
+    document.body.style.cursor = "grabbing";
+    const xRotationBase = cameraRef.current.xRotation;
+    const yRotationBase = cameraRef.current.yRotation;
+
+    let directionLock: "x" | "y" | undefined = undefined;
+
+    const handleMouseMove = (move: MouseEvent) => {
+      const dx = move.clientX - down.clientX;
+      let dy = move.clientY - down.clientY;
+
+      let yRotation = yRotationBase + (dx * Y_ROTATION_SPEED) / CANVAS_HEIGHT;
+
+      if (Math.abs(yRotation) % (2 * Math.PI) > Math.PI) {
+        dy = -1 * dy;
+      }
+
+      let xRotation = xRotationBase + (dy * X_ROTATION_SPEED) / CANVAS_WIDTH;
+      xRotation = Math.min(Math.PI / 2, xRotation);
+
+      console.log({ yRotation });
+
+      const xDistance = Math.abs(dx);
+      const yDistance = Math.abs(dy);
+
+      if (directionLock === "x") {
+        yRotation = yRotationBase;
+      } else if (directionLock === "y") {
+        xRotation = xRotationBase;
+      } else if (xDistance > 10 && xDistance > yDistance) {
+        directionLock = "y";
+      } else if (yDistance > 10 && yDistance > xDistance) {
+        directionLock = "x";
+      }
+
+      cameraRef.current = {
+        ...cameraRef.current,
+        xRotation,
+        yRotation,
+      };
+
+      render();
+    };
+
+    const handleMouseUp = () => {
+      // Set grabbing to false so the hover cursor on the canvas goes back to grab
+      setGrabbing(false);
+      // Also remove the cursor from the body which applies when we drag off the canvas
+      document.body.style.removeProperty("cursor");
+
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+  };
+
   return (
-    <canvas ref={render} width={CANVAS_WIDTH} height={CANVAS_HEIGHT}></canvas>
+    <canvas
+      ref={handleMount}
+      width={CANVAS_WIDTH}
+      height={CANVAS_HEIGHT}
+      onMouseDown={handleMouseDown}
+      style={{ cursor: grabbing ? "grabbing" : "grab" }}
+    ></canvas>
   );
 };
 
@@ -139,10 +360,8 @@ const V_FULL_SCREEN_QUAD = `
 const F_QUAD_RAY_CAST = `
   precision mediump float;
   uniform vec2 uResolution;
-  uniform vec3 uP00;
-  uniform vec3 uP01;
-  uniform vec3 uP10;
-  uniform vec3 uP11;
+  uniform vec3 uQuad[4];
+  uniform mat4 uProjection;
 
   // Interpolated position for this pixel
   varying vec4 vPosition;
@@ -150,8 +369,9 @@ const F_QUAD_RAY_CAST = `
   // Some colors for clarity:
   vec4 black = vec4(0.0, 0.0, 0.0, 1.0);
   vec4 magenta = vec4(1.0, 0.0, 1.0, 1.0);
-  
-  void paintPointWithinQuad(out vec4 fragColor, in vec3 pIntersection, in vec3 normal) {
+  float fov = ${CAMERA_FOV};
+
+  void paintPointWithinQuad(out vec4 fragColor, in vec3 pIntersection, in vec3 normal, in vec3 p00, in vec3 p01, in vec3 p10, in vec3 p11) {
 
     // Slab Test
     // —————————
@@ -177,13 +397,13 @@ const F_QUAD_RAY_CAST = `
 
     // Edge tests (Clockwise traversal)
     // p00->p01
-    float orientationA = dot(pIntersection - uP00, cross(uP01 - uP00, normal));
+    float orientationA = dot(pIntersection - p00, cross(p01 - p00, normal));
     // p01->p11
-    float orientationB = dot(pIntersection - uP01, cross(uP11 - uP01, normal));
+    float orientationB = dot(pIntersection - p01, cross(p11 - p01, normal));
     // p11->p10
-    float orientationC = dot(pIntersection - uP11, cross(uP10 - uP11, normal));
+    float orientationC = dot(pIntersection - p11, cross(p10 - p11, normal));
     // p10->p00
-    float orientationD = dot(pIntersection - uP10, cross(uP00 - uP10, normal));
+    float orientationD = dot(pIntersection - p10, cross(p00 - p10, normal));
 
    if (orientationA < 0.0 || orientationB < 0.0 || orientationC < 0.0 || orientationD < 0.0) {
       fragColor = black;
@@ -197,12 +417,32 @@ const F_QUAD_RAY_CAST = `
       vec2 uv = fragCoord/uResolution.xy;
 
       // Convert to normalized device coordinates
-      vec2 ndc = uv * 2.0 - 1.0;
+      vec2 ndc = (uv * 2.0 - 1.0) * fov;
 
-      vec3 anyPointOnPlane = uP00;
+      // Convert the quad uniform to a homogeneous matrix
+      vec4 hQuad[4];
+      hQuad[0] = vec4(uQuad[0], 1);
+      hQuad[1] = vec4(uQuad[1], 1);
+      hQuad[2] = vec4(uQuad[2], 1);
+      hQuad[3] = vec4(uQuad[3], 1);
+
+      // Then apply the camera projection to the homogenous quad. This should
+      // multiply each vec4 in the hQuad by the uProjection.
+      vec4 pQuad[4];
+      pQuad[0] = uProjection * hQuad[0];
+      pQuad[1] = uProjection * hQuad[1];
+      pQuad[2] = uProjection * hQuad[2];
+      pQuad[3] = uProjection * hQuad[3];
+
+      vec3 p00 = pQuad[0].xyz / pQuad[0].w;
+      vec3 p01 = pQuad[1].xyz / pQuad[1].w;
+      vec3 p10 = pQuad[2].xyz / pQuad[2].w;
+      vec3 p11 = pQuad[3].xyz / pQuad[3].w;
+
+      vec3 anyPointOnPlane = p00;
       vec3 rayOrigin = vec3(0.0);
       // n = (p10​ − p00​) × (p01​ − p00​)
-      vec3 normal = cross(uP10 - uP00, uP01 - uP00);
+      vec3 normal = cross(p10 - p00, p01 - p00);
       vec3 rayDirection = vec3(ndc, -1.0);
 
       // Ray-plane intersection test
@@ -234,7 +474,7 @@ const F_QUAD_RAY_CAST = `
       if (t < 0.0) {
         fragColor = black;
       } else {
-        paintPointWithinQuad(fragColor, pIntersection, normal);
+        paintPointWithinQuad(fragColor, pIntersection, normal, p00, p01, p10, p11);
       }
   }
 
